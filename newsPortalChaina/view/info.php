7ffc12e37eba0b04d8d4631dd9b79bbf6cfb634d
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2024-05-20 14:36:08 +0300

    Add files via upload

--- newsPortalChaina/view/info.php
+++ newsPortalChaina/view/info.php
@@ -1,16 +1,90 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> dalispa</title>
+        <title>dalispa</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggoyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
         crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="style.css">
-        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Добавлен метатег для мобильной адаптации -->
+        <style>
+            .navbar {
+            text-align: center;
+        }
+        .topmenu {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0;
+            margin: 0;
+            list-style: none;
+        }
+        .topmenu > li {
+            margin: 5px 0;
+        }
+        .topmenu > li > a {
+            display: block;
+            padding: 10px;
+            text-decoration: none;
+            color: #fff;
+            background: #93815E;
+            border-radius: 5px;
+            width: 100%;
+            text-align: center;
+        }
+        .submenu {
+            display: none;
+            list-style: none;
+            background: #93815E;
+            padding: 0;
+        }
+        .topmenu > li:hover .submenu {
+            display: block;
+        }
+        .submenu > li > a {
+            padding: 5px;
+            background: #93815E;
+            border-radius: 5px;
+        }
+        .divBox {
+            padding: 20px;
+        }
+        @media (min-width: 768px) {
+            .topmenu {
+                flex-direction: row;
+                justify-content: space-around;
+            }
+            .topmenu > li {
+                margin: 0;
+            }
+            .topmenu > li > a {
+                width: auto;
+            }
+            .submenu {
+                position: absolute;
+                display: none;
+            }
+            .topmenu > li:hover .submenu {
+                display: block;
+                background: #93815E;
+                position: absolute;
+                box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+            }
+            .submenu > li {
+                margin: 0;
+            }
+            .submenu > li > a {
+                width: 200px;
+            }
+        }
+        </style>
     </head>
     <body>
     <nav class="one">
+    <div style="text-align: right; background: #93815E;">
+    <a href="/newsPortalV26/"><img src="view/ukflag.png" alt="Flag Image"width="50" height="50"></a></div>
     <ul class="topmenu">
         <li><a href="#">类别<i class="fa fa-angle-down"></i></a>
             <ul class="submenu">
@@ -23,7 +97,7 @@
         <li><a href="./">斯塔迪莱特</a></li>
         <div style="text-align: right;">
     
-    <a href="/newsPortalV26/"><img src="view/ukflag.png" alt="Flag Image"width="50" height="50"></a>
+    
 </div>
     </ul>
     <div class="navbar">
@@ -40,7 +114,21 @@
             <p>今天就访问我们，体验不同之处！</p>
         </div>
     </section>
+    <div class="contact-info">
+    <p>地址： 116 Hung Vuong, Thanh Ha, 会安, 越南</p>
 
+    <p>电话: <a href="tel:+842353939333">+842353939333</a> 
+    <p> 热线: <a href="tel:+84386272471">+ 84 386 272 471</a> </p>
+    <p> 电子邮件: <a href="mailto:info@hoiandelicacyhotel.com">info@hoiandelicacyhotel.com</a></p>
+
+    <p>版权所有 © 2022。会安美食水疗酒店。 版权所有.</p>
+
+    <p>网站自豪地制作</p>
+</div>
+
+<div class="map-container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2032.042256291174!2d28.162532177360365!3d59.382322006777606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4694499a63b0b6e1%3A0x963e7adab71544a!2sSalon%20Pro!5e0!3m2!1set!2see!4v1715845200537!5m2!1set!2see" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
    
 
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
--- newsPortalChaina/view/services.php
+++ newsPortalChaina/view/services.php
@@ -4,12 +4,12 @@ class ViewServices
 
     public static function ServicesByCategory($arr)
     {
-        
         foreach ($arr as $value) {
             echo '<img src="data:image/jpeg;base64,' . base64_encode($value['picture']) . '" width=150 /><br>';
             echo "<h2>" . $value['chtitle'] . "</h2>";
+            echo "<p>价格: $" . $value['price'] . "</p>"; // Отображение цены
             Controller::commentsCount($value['id']);
-            echo "<a href='services?id=" . $value['id'] . "'>Edasi</a><br>";
+            echo "<a href='services?id=" . $value['id'] . "'>下一个</a><br>";
         }
     }
 
@@ -17,18 +17,19 @@ class ViewServices
     {
         foreach ($arr as $value) {
             echo "<li>" . $value['chtitle'];
+            echo "<p>价格: $" . $value['price'] . "</p>"; // Отображение цены
             Controller::commentsCount($value['id']);
-            echo "<a href='services?id=" . $value['id'] . "'>Edasi</a></li><br>";
+            echo "<a href='services?id=" . $value['id'] . "'>下一个</a></li><br>";
         }
     }
 
     public static function readServices($n)
     {
         echo "<h2>" . $n['chtitle'] . "</h2>";
+        echo "<p>价格: $" . $n['price'] . "</p>"; // Отображение цены
         Controller::commentsCountWithAnchor($n['id']);
         echo '<br><img src="data:image/jpeg;base64,' . base64_encode($n['picture']) . '" width=150/><br>';
         echo "<p>" . $n['chtext'] . "</p>";
     }
 
-   
 }
